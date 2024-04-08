5ce481f2b63f4dbb95ef262d17e4e24f61027e27
Maximillian Bjørn Mortensen <s236167@dtu.dk>
2024-04-08 17:02:52 +0200

    inputs

--- mp2_CLionProject/main.c
+++ mp2_CLionProject/main.c
@@ -9,6 +9,8 @@ struct Card{
     struct Card* nextCard;
 };
 
+char input[20];
+
 bool playPhase = false;
 
 char deck[2][52];
@@ -26,32 +28,115 @@ struct Card* F2;
 struct Card* F3;
 struct Card* F4;
 
-bool ld(char[]);
+char* findAgument();
+int findMethod();
+
+bool ld(char*);
 bool sw();
-bool sl();
+bool sl(char*);
 void sr();
-void sd(char[]);
-void qq();
+void sd(char*);
 void p();
 void q();
-bool move(char[]);
+bool move(char*);
 void u();
 bool r();
-void s();
-bool l();
+void s(char*);
+bool l(char*);
 
 void printBord();
 
 int main() {
-    char input[50];
-    while(true){
+    bool exit = false;
+    while(!exit) {
         printBord();
-        scanf("%s", input);
-        if(playPhase){
-
-        }else{
-
+        scanf("%20s", input);
+        int method = findMethod();
+        switch (method) {
+            case 1:
+                ld(findAgument());
+                break;
+            case 2:
+                sw();
+                break;
+            case 3:
+                sl(findAgument());
+                break;
+            case 4:
+                sr();
+                break;
+            case 5:
+                sd(findAgument());
+                break;
+            case 6:
+                exit = true;
+                break;
+            case 7:
+                p();
+                break;
+            case 8:
+                q();
+                break;
+            case 9:
+                move(findAgument());
+                break;
+            case 10:
+                u();
+                break;
+            case 11:
+                r();
+                break;
+            case 12:
+                s(findAgument());
+                break;
+            case 13:
+                l(findAgument());
+                break;
         }
     }
     return 0;
+}
+
+char* findAgument(){
+
+}
+int findMethod(){
+
+}
+
+bool ld(char* filename){
+
+}
+bool sw(){
+
+}
+bool sl(char* num){
+
+}
+void sr(){
+
+}
+void sd(char* filename){
+
+}
+void p(){
+
+}
+void q(){
+
+}
+bool move(char* move){
+
+}
+void u(){
+
+}
+bool r(){
+
+}
+void s(char* filename){
+
+}
+bool l(char* filename){
+
 }
