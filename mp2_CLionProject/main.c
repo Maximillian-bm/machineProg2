b914105faa0d136958ce1cef62bda315d02c374f
Maximillian Bjørn Mortensen <s236167@dtu.dk>
2024-04-08 15:21:38 +0200

    global var and methods named

--- mp2_CLionProject/main.c
+++ mp2_CLionProject/main.c
@@ -9,6 +9,33 @@ struct Card{
     struct Card* nextCard;
 };
 
+struct Card* C1;
+struct Card* C2;
+struct Card* C3;
+struct Card* C4;
+struct Card* C5;
+struct Card* C6;
+struct Card* C7;
+
+struct Card* F1;
+struct Card* F2;
+struct Card* F3;
+struct Card* F4;
+
+bool ld(char[]);
+bool sw();
+bool sl();
+void sr();
+void sd(char[]);
+void qq();
+void p();
+void q();
+bool move(char[]);
+void u();
+bool r();
+void s();
+bool l();
+
 int main() {
     printf("Hello, World!\n");
     return 0;
