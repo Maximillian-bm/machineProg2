27807d8b6ab1c5848be1368452d9c8cd7f2a6f8c
Maximillian Bjørn Mortensen <s236167@dtu.dk>
2024-04-10 10:53:44 +0200

    ready for development

--- mp2_CLionProject/main.c
+++ mp2_CLionProject/main.c
@@ -15,139 +15,146 @@ struct CardInDeck{
     int num;
 };
 
-char input[20];
+struct Board{
 
-bool playPhase = false;
+    char input[20];
 
-struct CardInDeck deck[52];
+    bool playPhase;
 
-struct Card* C1;
-struct Card* C2;
-struct Card* C3;
-struct Card* C4;
-struct Card* C5;
-struct Card* C6;
-struct Card* C7;
+    struct CardInDeck deck[52];
 
-struct Card* F1;
-struct Card* F2;
-struct Card* F3;
-struct Card* F4;
+    struct Card* c[7];
 
-char* findAgument(int);
-int findMethod();
+    struct Card* f[4]
+};
 
-bool ld(char*);
-bool sw();
-bool sl(char*);
-void sr();
-void sd(char*);
-void p();
-void q();
-bool move(char*);
-void u();
-bool r();
-void s(char*);
-bool l(char*);
+char* findAgument(struct Board, int);
+int findMethod(struct Board);
 
-void printBord();
+bool ld(struct Board, char*);
+bool sw(struct Board);
+bool sl(struct Board, char*);
+void sr(struct Board);
+void sd(struct Board, char*);
+void p(struct Board);
+void q(struct Board);
+bool move(struct Board, char*);
+void u(struct Board);
+bool r(struct Board);
+void s(struct Board, char*);
+bool l(struct Board, char*);
+
+void printBord(struct Board);
 
 int main() {
+
+    struct Board board;
+
+    board.playPhase = false;
+
     bool exit = false;
+
     while(!exit) {
-        printBord();
-        scanf("%20s", input);
-        int method = findMethod();
+
+        printBord(board);
+
+        scanf("%20s", board.input);
+
+        int method = findMethod(board);
+
         switch (method) {
             case 1:
-                ld(findAgument(1));
+                ld(board, findAgument(board,1));
                 break;
             case 2:
-                sw();
+                sw(board);
                 break;
             case 3:
-                sl(findAgument(3));
+                sl(board, findAgument(board,3));
                 break;
             case 4:
-                sr();
+                sr(board);
                 break;
             case 5:
-                sd(findAgument(5));
+                sd(board, findAgument(board,5));
                 break;
             case 6:
                 exit = true;
                 break;
             case 7:
-                p();
+                p(board);
                 break;
             case 8:
-                q();
+                q(board);
                 break;
             case 9:
-                move(findAgument(9));
+                move(board,findAgument(board,9));
                 break;
             case 10:
-                u();
+                u(board);
                 break;
             case 11:
-                r();
+                r(board);
                 break;
             case 12:
-                s(findAgument(12));
+                s(board,findAgument(board,12));
                 break;
             case 13:
-                l(findAgument(13));
+                l(board,findAgument(board,13));
                 break;
         }
     }
     return 0;
 }
 
-char* findAgument(int aguNum){
+char* findAgument(struct Board board, int aguNum){
 // in the "input" find and return the agument.
 // in the case that the agunum is 1, 5, 12 or 13, return the filename between < > in "input"
 // in the case that agunum is 3 return the number in "input"
 // in the case of 9 return the whole string of "input"
 // any returns should be a char* to a new char array
 }
-int findMethod(){
+int findMethod(struct Board board){
 // return the number assotiated with the command saved in "input"
 // if input is not valid return 0
 }
 
-bool ld(char* filename){
+bool ld(struct Board board, char* filename){
 
 }
-bool sw(){
+bool sw(struct Board board){
 
 }
-bool sl(char* num){
+bool sl(struct Board board, char* num){
 
 }
-void sr(){
+void sr(struct Board board){
 
 }
-void sd(char* filename){
+void sd(struct Board board, char* filename){
 
 }
-void p(){
+void p(struct Board board){
 
 }
-void q(){
+void q(struct Board board){
 
 }
-bool move(char* move){
+bool move(struct Board board, char* move){
 
 }
-void u(){
+void u(struct Board board){
 
 }
-bool r(){
+bool r(struct Board board){
 
 }
-void s(char* filename){
+void s(struct Board board, char* filename){
 
 }
-bool l(char* filename){
+bool l(struct Board board, char* filename){
+
+}
+void printBord(struct Board board){
 
 }
