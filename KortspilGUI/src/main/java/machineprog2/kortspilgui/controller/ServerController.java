cacb3e6afd9dae2267ef4021750479d215a4f62d
GustavAggeboe <gustavaggeboe@hotmail.com>
2024-04-22 18:56:04 +0200

    Create ServerController.java

---
+++ KortspilGUI/src/main/java/machineprog2/kortspilgui/controller/ServerController.java
@@ -0,0 +1,50 @@
+package machineprog2.kortspilgui.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerController {
+    public static void main(String[] args) {
+        try {
+            // Create a ServerSocket and bind it to a port
+            ServerSocket serverSocket = new ServerSocket(1312);
+            System.out.println("Server started. Waiting for client...");
+
+            // Accept client connection
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected.");
+
+            // Get input and output streams
+            InputStream inputStream = clientSocket.getInputStream();
+            OutputStream outputStream = clientSocket.getOutputStream();
+
+            // Continuously send and receive data
+            while (true) {
+                // Receive data from client
+                byte[] buffer = new byte[1024];
+                int bytesRead = inputStream.read(buffer);
+                if (bytesRead == -1) {
+                    break; // Connection closed by client
+                }
+                String receivedMessage = new String(buffer, 0, bytesRead);
+                System.out.println("Received from client: " + receivedMessage);
+
+                // Send response to client
+                String responseMessage = "Hello from server!";
+                outputStream.write(responseMessage.getBytes());
+                System.out.println("Sent to client: " + responseMessage);
+            }
+
+            // Close streams and socket
+            inputStream.close();
+            outputStream.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
